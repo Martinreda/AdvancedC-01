0edf286f7d59a41fb06f3ea7c87c75b02189de3d
martin reda <martinreda999@outlook.com>
2025-08-10 04:11:45 +0300

    Assignment

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Range<int> intRange = new Range<int>(10, 20);
+            Console.WriteLine(intRange.IsInRange(15)); 
+            Console.WriteLine(intRange.IsInRange(25)); 
+            Console.WriteLine(intRange.Length());      
+
+            Range<double> doubleRange = new Range<double>(1.5, 4.5);
+            Console.WriteLine(doubleRange.IsInRange(3.2)); 
+            Console.WriteLine(doubleRange.Length());
         }
     }
 }
